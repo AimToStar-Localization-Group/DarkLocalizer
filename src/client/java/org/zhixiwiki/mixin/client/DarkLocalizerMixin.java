91b2e027dcdc7050213456ae1ecb77c562e9abf6
HydroH <iodizon@163.com>
2023-08-28 23:32:03 +0800

    修复与其它tooltip模组冲突问题

--- src/client/java/org/zhixiwiki/mixin/client/DarkLocalizerMixin.java
+++ src/client/java/org/zhixiwiki/mixin/client/DarkLocalizerMixin.java
@@ -2,23 +2,25 @@ package org.zhixiwiki.mixin.client;
 
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.zhixiwiki.Translations;
 
 import java.util.List;
 
 @Mixin(ItemStack.class)
 public class DarkLocalizerMixin {
-    @Inject(method = "getTooltip", at = @At("RETURN"), cancellable = true)
-    private void translateTooltip(@Nullable PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> info) {
-        List<Text> tooltip = info.getReturnValue();
-        tooltip.replaceAll(x -> Translations.translations.getOrDefault(x.getString(), x));
-        info.setReturnValue(tooltip);
+    @Inject(method = "getTooltip", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void translateTooltip(@Nullable PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> cir, List<Text> list) {
+        list.replaceAll(x -> Translations.translations.getOrDefault(x.getString(), x));
     }
 }
